9086aa915a277f3e2d689ee386a6f6e29fc7b6e9
Quinn Slack <qslack@qslack.com>
2016-09-21 04:47:01 -0700

    store documents when we get textDocument/didOpen and other doc notifs

--- src/language-server.ts
+++ src/language-server.ts
@@ -18,6 +18,10 @@ import {
     SymbolInformation, SymbolKind, Range, RequestType
 } from 'vscode-languageserver';
 
+import {
+	TextDocumentChangeEvent,
+} from 'vscode-languageserver-types';
+
 import TypeScriptService from './typescript-service';
 import Connection from './connection';
 
@@ -76,6 +80,14 @@ var server = net.createServer(function (socket) {
         console.log('shutdown...');
 	return [];
     });
+
+	// Keep the documents store updated (upon receiving
+	// textDocument/didOpen, etc., notifs).
+	documents.listen(connection.connection);
+
+	documents.onDidOpen((e: TextDocumentChangeEvent) => {
+		console.log(`textDocument/didOpen ${e.document.uri}`);
+	});
 
     connection.connection.onWorkspaceSymbol((params: WorkspaceSymbolParams): SymbolInformation[] => {
         try {
