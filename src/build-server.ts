699f834c5b4e04e2170c1a9f0471d52a63a1d530
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-10-03 14:49:28 +0300

    Add changing path to the folder which contains tsconfig.json

--- src/build-server.ts
+++ src/build-server.ts
@@ -7,8 +7,7 @@ var path = require('path');
 var os = require('os');
 
 var program = require('commander');
-
-import {StreamMessageReader} from 'vscode-languageserver';
+var glob = require("glob")
 
 var server = net.createServer(function (socket) {
 
@@ -20,13 +19,41 @@ var server = net.createServer(function (socket) {
 
 	socket.on('data', function (data) {
 		let dataInfo = processData(data.toString());
-
 		if (dataInfo) {
 			if (dataInfo.json.method == 'initialize') {
-				console.error("Inside initialize method = ");
-				//Add initialization procedures here
+                let root = dataInfo.json.params.rootPath;
+
+				//find tsconfig.json file and update rootPath
+				try {
+					process.chdir(root);
+					glob("**/tsconfig.json", function (err, files) {
+						if (err) throw err;
+						let resolvedFiles = [];
+						files = files.forEach(function (match) {
+							if (match.indexOf("node_modules") == -1) {
+								resolvedFiles.push(match);
+							}
+							return path.relative(root, match);
+						});
+
+                        //TODO process multiple tsconfig.json in one project
+						//For now we process only 1 folder with tsconfig.json
+						let resolvedFile = resolvedFiles[0];
+						let newPath = path.join(root, path.dirname(resolvedFile));
+						dataInfo.json.params.rootPath = newPath;
+
+						//Write data with updated root path to client
+						let dataBuffer = new Buffer(JSON.stringify(dataInfo.json), "utf-8");
+						client.write(new Buffer(updateHeaders(dataBuffer.byteLength, dataInfo.headers), "utf-8"));
+						client.write(dataBuffer);
+					})
+
+				} catch (error) {
+					console.error("Error in config file processing");
+				}
+			} else {
+				client.write(data);
 			}
-			client.write(data);
 		}
 	});
 
@@ -35,6 +62,34 @@ var server = net.createServer(function (socket) {
 	});
 
 });
+
+function updateHeaders(length, headers) {
+	let CRLF = '\r\n';
+	let headersData = headers.split(CRLF);
+	let result = "";
+
+	headersData.forEach(function (header) {
+		if (header.length > 0) {
+			var index = header.indexOf(':');
+			if (index === -1) {
+				throw new Error('Message header must separate key and value using :');
+			}
+			var key = header.substr(0, index);
+			var value = header.substr(index + 1).trim();
+
+			if (key == "Content-Length") {
+				result = result + "Content-Length:" + length + CRLF;
+			} else {
+				result = result + header;
+			}
+		} else {
+			result = result + header + CRLF;
+		}
+	});
+
+	return result;
+
+}
 
 function processData(data: string) {
 	let jsonStart = data.indexOf('{');
