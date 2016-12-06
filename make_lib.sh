a37967df8ee79e5b03d00d8bc19206ca8f757f0c
Beyang Liu <beyang@sourcegraph.com>
2016-12-06 11:05:12 -0800

    npm run lib

--- make_lib.sh
+++ make_lib.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-./node_modules/.bin/tsc --declaration --outDir ./src && git add -A && git commit -m'[GENERATED FILES]' && git push -f origin HEAD:build
+if ! git diff-index --quiet HEAD --; then
+    echo ""
+    echo "!!! Working directory is dirty, aborting."
+    echo ""
+    git status
+    exit 1
+fi
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]; then
+    echo ""
+    echo "!!! Must be on master branch"
+    echo ""
+    exit 1
+fi
+
+git branch build && git checkout build && ./node_modules/.bin/tsc --declaration --outDir ./src && git add :/src && git commit -m'[GENERATED FILES]' && git push -f origin HEAD:build && git checkout master
