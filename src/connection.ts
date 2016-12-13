eea7ca341defda2eb1c8f4a64b9b9a5b319912b1
Beyang Liu <beyang@sourcegraph.com>
2016-12-12 22:39:24 -0800

    simplify connection handlers using async/await and ensure uncaught exceptions are always logged

--- src/connection.ts
+++ src/connection.ts
@@ -57,7 +57,7 @@ export function newConnection(input: any, output: any): IConnection {
 }
 
 export function registerLanguageHandler(connection: IConnection, strict: boolean, handler: LanguageHandler): void {
-	connection.onRequest(rt.InitializeRequest.type, (params: InitializeParams): Promise<InitializeResult> => {
+	connection.onRequest(rt.InitializeRequest.type, async (params: InitializeParams): Promise<InitializeResult> => {
 		console.error('initialize', params.rootPath);
 		let remoteFs: fs.FileSystem;
 		if (strict) {
@@ -66,7 +66,7 @@ export function registerLanguageHandler(connection: IConnection, strict: boolean
 			remoteFs = new fs.LocalFileSystem(util.uri2path(params.rootPath));
 		}
 		try {
-			return handler.initialize(params, remoteFs, strict);
+			return await handler.initialize(params, remoteFs, strict);
 		} catch (e) {
 			console.error(params, e);
 			return Promise.reject(e);
@@ -80,111 +80,81 @@ export function registerLanguageHandler(connection: IConnection, strict: boolean
 	connection.onDidSaveTextDocument((params: DidSaveTextDocumentParams) => handler.didSave(params));
 	connection.onDidCloseTextDocument((params: DidCloseTextDocumentParams) => handler.didClose(params));
 
-	connection.onRequest(rt.WorkspaceSymbolsRequest.type, (params: rt.WorkspaceSymbolParamsWithLimit): Promise<SymbolInformation[]> => {
+	connection.onRequest(rt.WorkspaceSymbolsRequest.type, async (params: rt.WorkspaceSymbolParamsWithLimit): Promise<SymbolInformation[]> => {
 		const enter = new Date().getTime();
-		return new Promise<SymbolInformation[]>((resolve, reject) => {
-			const init = new Date().getTime();
-			try {
-				return handler.getWorkspaceSymbols(params).then((result) => {
-					const exit = new Date().getTime();
-					console.error('workspace/symbol', params.query, 'total', (exit - enter) / 1000.0, 'busy', (exit - init) / 1000.0, 'wait', (init - enter) / 1000.0);
-					return resolve(result || []);
-				});
-			} catch (e) {
-				console.error(params, e);
-				return resolve([]);
-			}
-		});
+		try {
+			const result = await handler.getWorkspaceSymbols(params);
+			const exit = new Date().getTime();
+			console.error('workspace/symbol', params.query, 'total', (exit - enter) / 1000.0, 'busy', (exit - enter) / 1000.0);
+			return Promise.resolve(result || []);
+		} catch (e) {
+			console.error(params, e);
+			return Promise.reject(e);
+		}
 	});
 
-	connection.onRequest(rt.DocumentSymbolRequest.type, (params: DocumentSymbolParams): Promise<SymbolInformation[]> => {
+	connection.onRequest(rt.DocumentSymbolRequest.type, async (params: DocumentSymbolParams): Promise<SymbolInformation[]> => {
 		const enter = new Date().getTime();
-		return new Promise<SymbolInformation[]>((resolve, reject) => {
-			const init = new Date().getTime();
-			try {
-				return handler.getDocumentSymbol(params).then((result) => {
-					const exit = new Date().getTime();
-					console.error('textDocument/documentSymbol', "", 'total', (exit - enter) / 1000.0, 'busy', (exit - init) / 1000.0, 'wait', (init - enter) / 1000.0);
-					return resolve(result || []);
-				});
-			} catch (e) {
-				console.error(params, e);
-				return resolve([]);
-			}
-		});
+		try {
+			const result = await handler.getDocumentSymbol(params);
+			const exit = new Date().getTime();
+			console.error('textDocument/documentSymbol', 'total', (exit - enter) / 1000.0, 'busy', (exit - enter) / 1000.0);
+			return Promise.resolve(result || []);
+		} catch (e) {
+			console.error(params, e);
+			return Promise.reject(e);
+		}
 	});
 
-	connection.onRequest(rt.WorkspaceReferenceRequest.type, (params: rt.WorkspaceReferenceParams): Promise<rt.ReferenceInformation[]> => {
+	connection.onRequest(rt.WorkspaceReferenceRequest.type, async (params: rt.WorkspaceReferenceParams): Promise<rt.ReferenceInformation[]> => {
 		const enter = new Date().getTime();
-		return new Promise<rt.ReferenceInformation[]>((resolve, reject) => {
-			const init = new Date().getTime();
-			try {
-				return handler.getWorkspaceReference(params).then((result) => {
-					const exit = new Date().getTime();
-					console.error('workspace/reference', 'total', (exit - enter) / 1000.0, 'busy', (exit - init) / 1000.0, 'wait', (init - enter) / 1000.0);
-					return resolve(result || []);
-				});
-			} catch (e) {
-				console.error(params, e);
-				return resolve([]);
-			}
-		});
+		try {
+			const result = await handler.getWorkspaceReference(params);
+			const exit = new Date().getTime();
+			console.error('workspace/reference', 'total', (exit - enter) / 1000.0, 'busy', (exit - enter) / 1000.0);
+			return Promise.resolve(result || []);
+		} catch (e) {
+			console.error(params, e);
+			return Promise.reject(e);
+		}
 	});
 
-
-	connection.onDefinition((params: TextDocumentPositionParams): Promise<Definition> => {
+	connection.onDefinition(async (params: TextDocumentPositionParams): Promise<Definition> => {
 		const enter = new Date().getTime();
-		return new Promise<Definition>((resolve, reject) => {
-			try {
-				const init = new Date().getTime();
-				handler.getDefinition(params).then((result) => {
-					const exit = new Date().getTime();
-					console.error('definition', params.textDocument.uri, params.position.line, params.position.character, 'total', (exit - enter) / 1000.0, 'busy', (exit - init) / 1000.0, 'wait', (init - enter) / 1000.0);
-					return resolve(result || []);
-				}, (e) => {
-					return reject(e);
-				});
-			} catch (e) {
-				console.error(params, e);
-				return resolve([]);
-			}
-		});
+		try {
+			const result = await handler.getDefinition(params);
+			const exit = new Date().getTime();
+			console.error('definition', params.textDocument.uri, params.position.line, params.position.character, 'total', (exit - enter) / 1000.0, 'busy', (exit - enter) / 1000.0);
+			return Promise.resolve(result || []);
+		} catch (e) {
+			console.error(params, e);
+			return Promise.reject(e);
+		}
 	});
 
-	connection.onHover((params: TextDocumentPositionParams): Promise<Hover> => {
+	connection.onHover(async (params: TextDocumentPositionParams): Promise<Hover> => {
 		const enter = new Date().getTime();
-		return new Promise<Hover>((resolve, reject) => {
-			const init = new Date().getTime();
-			try {
-				handler.getHover(params).then((hover) => {
-					const exit = new Date().getTime();
-					console.error('hover', params.textDocument.uri, params.position.line, params.position.character, 'total', (exit - enter) / 1000.0, 'busy', (exit - init) / 1000.0, 'wait', (init - enter) / 1000.0);
-					resolve(hover || { contents: [] });
-				}, (e) => {
-					return reject(e);
-				});
-			} catch (e) {
-				console.error(params, e);
-				resolve({ contents: [] });
-			}
-		});
+		try {
+			const result = await handler.getHover(params);
+			const exit = new Date().getTime();
+			console.error('hover', params.textDocument.uri, params.position.line, params.position.character, 'total', (exit - enter) / 1000.0, 'busy', (exit - enter) / 1000.0);
+			return Promise.resolve(result || { contents: [] });
+		} catch (e) {
+			console.error(params, e);
+			Promise.reject(e);
+		}
 	});
 
-	connection.onReferences((params: ReferenceParams): Promise<Location[]> => {
-		return new Promise<Location[]>((resolve, reject) => {
-			const enter = new Date().getTime();
-			const init = new Date().getTime();
-			try {
-				handler.getReferences(params).then((result) => {
-					const exit = new Date().getTime();
-					console.error('references', params.textDocument.uri, params.position.line, params.position.character, 'found', result.length, 'total', (exit - enter) / 1000.0, 'busy', (exit - init) / 1000.0, 'wait', (init - enter) / 1000.0);
-					return resolve(result || []);
-				}
-				);
-			} catch (e) {
-				console.error(params, e);
-				return resolve([]);
-			}
-		});
+	connection.onReferences(async (params: ReferenceParams): Promise<Location[]> => {
+		const enter = new Date().getTime();
+		try {
+			const result = await handler.getReferences(params);
+			const exit = new Date().getTime();
+			console.error('references', params.textDocument.uri, params.position.line, params.position.character, 'found', result.length, 'total', (exit - enter) / 1000.0, 'busy', (exit - enter) / 1000.0);
+			return Promise.resolve(result || []);
+		} catch (e) {
+			console.error(params, e);
+			return Promise.reject(e);
+		}
 	});
 }
