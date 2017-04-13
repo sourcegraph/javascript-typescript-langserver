0454d28455b352adf5b6373a6329371b5c04dffb
Alex Saveliev <lyolik@codeminders.com>
2017-04-13 13:42:53 +0300

    adjusted unit test

--- src/test/project-manager.test.ts
+++ src/test/project-manager.test.ts
@@ -2,28 +2,38 @@ import * as chai from 'chai';
 import chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const assert = chai.assert;
-import { TypeScriptService } from '../typescript-service';
-import { initializeTypeScriptService, shutdownTypeScriptService, TestContext } from './typescript-service-helpers';
+import { FileSystemUpdater } from '../fs';
+import { InMemoryFileSystem } from '../memfs';
+import { ProjectManager } from '../project-manager';
+import { MapFileSystem } from './fs-helpers';
+
+function initialize(files: Map<string, string>): ProjectManager {
+	const memfs = new InMemoryFileSystem('/');
+	const localfs = new MapFileSystem(files);
+	const updater = new FileSystemUpdater(localfs, memfs);
+	return new ProjectManager('/', memfs, updater, true);
+}
 
 describe('ProjectManager', () => {
-	describe('getPackageName()', function (this: TestContext) {
-		before(async function (this: TestContext) {
-			await initializeTypeScriptService(
-				(client, options) => new TypeScriptService(client, options),
+
+	let projectManager: ProjectManager;
+
+	describe('getPackageName()', () => {
+		before(() => {
+			projectManager = initialize(
 				new Map([
 					['file:///package.json', '{"name": "package-name-1"}'],
 					['file:///subdirectory-with-tsconfig/package.json', '{"name": "package-name-2"}'],
 					['file:///subdirectory-with-tsconfig/src/tsconfig.json', '{}'],
 					['file:///subdirectory-with-tsconfig/src/dummy.ts', '']
-				])).apply(this);
-			await this.service.projectManager.ensureOwnFiles();
-		} as any);
-		after(shutdownTypeScriptService as any);
-		it('should resolve package name when package.json is at the same level', function (this: TestContext) {
-			assert.equal(this.service.projectManager.getConfiguration('').getPackageName(), 'package-name-1');
-		} as any);
-		it('should resolve package name when package.json is at the upper level', function (this: TestContext) {
-			assert.equal(this.service.projectManager.getConfiguration('subdirectory-with-tsconfig/src/dummy.ts').getPackageName(), 'package-name-2');
-		} as any);
-	} as any);
+				]));
+			projectManager.ensureAllFiles();
+		});
+		it('should resolve package name when package.json is at the same level', () => {
+			assert.equal(projectManager.getConfiguration('').getPackageName(), 'package-name-1');
+		});
+		it('should resolve package name when package.json is at the upper level', () => {
+			assert.equal(projectManager.getConfiguration('subdirectory-with-tsconfig/src/dummy.ts').getPackageName(), 'package-name-2');
+		});
+	});
 });
