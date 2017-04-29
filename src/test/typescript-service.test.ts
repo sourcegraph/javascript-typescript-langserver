86f7223ffbc4635e18868ec43536abfc612890ac
Felix Becker <felix.b@outlook.com>
2017-04-29 16:40:43 +0200

    Test different root URIs

--- src/test/typescript-service.test.ts
+++ src/test/typescript-service.test.ts
@@ -3,5 +3,9 @@ import {TypeScriptService} from '../typescript-service';
 import {describeTypeScriptService} from './typescript-service-helpers';
 
 describe('TypeScriptService', () => {
-	describeTypeScriptService((client, options) => new TypeScriptService(client, options));
+	for (const rootUri of ['file:///', 'file:///c:/foo/bar/', 'file:///foo/bar/']) {
+		describe(`rootUri ${rootUri}`, () => {
+			describeTypeScriptService((client, options) => new TypeScriptService(client, options), undefined, rootUri);
+		});
+	}
 });
