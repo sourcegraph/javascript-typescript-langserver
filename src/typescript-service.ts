1237ffc2bce8e3fa09f286751f385a67368062a3
Felix Becker <felix.b@outlook.com>
2017-05-09 13:00:42 +0200

    Revert "Revert "Replace createConfigurations() with event API (#233)""
    This reverts commit 3a91ab8ceb2c67a7cac4c70420a149832a945fa9.

--- src/typescript-service.ts
+++ src/typescript-service.ts
@@ -550,7 +550,6 @@ export class TypeScriptService {
 			.mergeMap(uri => this.updater.ensure(uri, span))
 			.toArray()
 			.mergeMap<any, SymbolInformation>(() => {
-				this.projectManager.createConfigurations();
 				span.log({ event: 'fetched package files' });
 
 				// Search symbol in configuration
