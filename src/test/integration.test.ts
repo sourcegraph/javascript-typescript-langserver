ba09c84a1177ab7bebc82781788b4936c20fc95f
Tom van Ommeren <tom.vanommeren@gmail.com>
2017-05-22 20:46:57 +0200

    lint fix

--- src/test/integration.test.ts
+++ src/test/integration.test.ts
@@ -1,5 +1,5 @@
 import * as chai from 'chai';
-import * as sinon from 'Sinon';
+import * as sinon from 'sinon';
 import glob = require('glob');
 import { RemoteLanguageClient } from '../lang-handler';
 import { TypeScriptService } from '../typescript-service';
