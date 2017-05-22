c36793bc25ecd39d5338f19f87ba0c51ce6a43e5
Tom van Ommeren <tom.vanommeren@gmail.com>
2017-05-23 00:15:00 +0200

    reset spy before opening file

--- src/test/integration.test.ts
+++ src/test/integration.test.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai';
 import * as sinon from 'sinon';
 import glob = require('glob');
-import { RemoteLanguageClient } from '../lang-handler';
+import { LanguageClient, RemoteLanguageClient } from '../lang-handler';
 import { TypeScriptService } from '../typescript-service';
 import { path2uri } from '../util';
 import chaiAsPromised = require('chai-as-promised');
@@ -10,12 +10,13 @@ chai.use(chaiAsPromised);
 const assert = chai.assert;
 
 describe.only('running against this project', () => {
-	let client: any;
+	let client: { [K in keyof LanguageClient]: LanguageClient[K] & sinon.SinonStub };
 	let service: any;
 	let rootUri: string;
 
 	const rootPath = process.cwd();
 	const filePaths = glob.sync('src/*.ts');
+	const fs = new LocalFileSystem(rootPath);
 
 	before(async () => {
 		client = sinon.createStubInstance(RemoteLanguageClient);
@@ -33,10 +34,10 @@ describe.only('running against this project', () => {
 	for (const filePath of filePaths) {
 		it('should get no diagnostics on didOpen ' + filePath, async () => {
 			const fileUri = path2uri(rootPath, filePath);
-			const fs = new LocalFileSystem(rootPath);
 			const fileContent = await fs.getTextDocumentContent(fileUri);
 
 			assert.isAtLeast(fileContent.length, 1);
+			client.textDocumentPublishDiagnostics.resetHistory();
 
 			await service.textDocumentDidOpen({
 				textDocument: {
