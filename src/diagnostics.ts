78db06af97d4f0e3d27c71a431182ffa7163b318
Tom van Ommeren <tom.vanommeren@gmail.com>
2017-09-21 08:23:36 +0200

    fix: TS diagnostics fields are now optional

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,17 +1,20 @@
-
 import * as ts from 'typescript';
-import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver';
+import { Diagnostic, DiagnosticSeverity, Range } from 'vscode-languageserver';
 
 /**
  * Converts a TypeScript Diagnostic to an LSP Diagnostic
  */
 export function convertTsDiagnostic(diagnostic: ts.Diagnostic): Diagnostic {
 	const text = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-	return {
-		range: {
+	let range: Range = { start: {character: 0, line: 0}, end: {character: 0, line: 0} };
+	if (diagnostic.file && diagnostic.start && diagnostic.length) {
+		range = {
 			start: diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start),
 			end: diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start + diagnostic.length)
-		},
+		};
+	}
+	return {
+		range,
 		message: text,
 		severity: convertDiagnosticCategory(diagnostic.category),
 		code: diagnostic.code,
